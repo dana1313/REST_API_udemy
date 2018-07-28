ed0e5e4949c9031db6ad837105c27f383d69a0c1
darynamal <dm@aisc.io>
2018-07-28 21:37:02 +0300

    created run.py file and changed references

--- app.py
+++ app.py
@@ -15,14 +15,9 @@ app.secret_key = 'jose'
 
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)
 
 #items = []
-
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList,'/items')
